e70069c0e8be09df1fdc92277eeafd12c7e5bd96
RaySoham1022 <112944156+RaySoham1022@users.noreply.github.com>
2023-09-12 20:54:18 +0530

    Chat Bot Application

--- Task 4_Chat Bot/chatbot.py
+++ Task 4_Chat Bot/chatbot.py
@@ -48,33 +48,3 @@ while True:
 
 
 
-
-
-
-
-
-
-
-
-
-# import openai
-
-# openai.api_key = "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"
-
-# model_engine = "text-davinci-003"
-# prompt = "Who is Prime Minister of India ?"
-
-# complete = openai.Completion.create(
-#     engine = model_engine,
-#     prompt = prompt,
-#     max_tokens = 1024,
-#     n = 1,
-#     stop = None,
-#     temperature = 0.5,
-# )
-
-# response = complete.choices[0].text
-# print(response)
-
-
-
